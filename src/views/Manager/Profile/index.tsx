be8182682bb601761693b4e35f5a7b686e448410
ngocanh <ngocanh.pham19042001@gmail.com>
2025-06-03 08:00:53 +0700

    features/profile-user

--- src/views/Manager/Profile/index.tsx
+++ src/views/Manager/Profile/index.tsx
@@ -8,6 +8,7 @@ import useAuth from "@/hooks/useAuth";
 import { ROLE_LABELS, RoleUser } from "@/constants/taskStatus";
 import { useState, FormEvent, ChangeEvent, useEffect } from "react";
 import useNotification from "@/hooks/useNotification";
+import { updateUserProfile } from "@/services/user-service";
 
 interface DetailItemProps {
     label: string;
@@ -39,20 +40,20 @@ const getRoleLabel = (role: RoleUser | null | undefined): string => {
 
 interface ProfileFormData {
   full_name: string;
-  sex: string | null;
+  sex: string;
   date_of_birth: Dayjs | null;
   avatar_url: File | null;
   address: string | null;
   role: string;
   address_work: string | null;
-  phone_number: string | null;
+  phone_number: string;
   email: string | null;
 }
 
 function ProfileManager (){
     const today = dayjs();
     const eighteenYearsAgo = today.subtract(18, 'year');
-    const { profile } = useAuth();
+    const { userId, profile } = useAuth();
     const notify = useNotification();
     const [formData, setFormData] = useState<ProfileFormData>({
         full_name: '', sex: '', date_of_birth: null, avatar_url: null,
@@ -60,6 +61,8 @@ function ProfileManager (){
     });
     const [errors, setErrors] = useState<Partial<Record<'full_name' | 'phone_number' | 'date_of_birth' | 'sex', string>>>({});
     const [avatarPreview, setAvatarPreview] = useState<string | null>(null);
+
+    const phoneRegex = /^(0|\+84)(3[2-9]|5[6|8|9]|7[0|6-9]|8[1-5]|9[0-9])[0-9]{7}$/;
 
     const handleCustomInputChange = (name: string, value: string | Dayjs | null | number) => {
         if (Object.prototype.hasOwnProperty.call(formData, name)) {
@@ -71,6 +74,39 @@ function ProfileManager (){
         }));
 
         if (validName === 'full_name' || validName === 'phone_number' || validName === 'date_of_birth' || validName === 'sex') {
+            if(validName === 'phone_number' && typeof value === 'string'){
+                const phone = value.replace(/\s|-/g, '');
+                if (!/^\d+$/.test(phone)) {
+                    setErrors(prev => ({
+                        ...prev,
+                        phone_number: 'Số điện thoại chỉ chứa số',
+                    }));
+                    return;
+                }
+                if(phone.startsWith('0') && phone.length !== 10){
+                    setErrors(prev => ({
+                        ...prev,
+                        phone_number: 'Số điện thoại phải có 10 chữ số (nếu bắt đầu bằng 0)',
+                    }));
+                    return;
+                }
+
+                if(phone.startsWith('+84') && (phone.length < 11 || phone.length > 12)){
+                    setErrors(prev => ({
+                        ...prev,
+                        phone_number: 'Số điện thoại phải có 11-12 chữ số (nếu bắt đầu bằng +84)',
+                    }));
+                    return;
+                }
+
+                if(!phoneRegex.test(phone)){
+                    setErrors(prev => ({
+                        ...prev,
+                        phone_number: 'Số điện thoại không đúng định dạng (bắt đầu từ +84|03|05|07|08|09',
+                    }));
+                    return;
+                }
+            }
             if (errors[validName as 'full_name' | 'phone_number' | 'date_of_birth' | 'sex']) {
                 setErrors(prev => {
                     const newErrors = { ...prev };
@@ -84,6 +120,18 @@ function ProfileManager (){
         }
     };
 
+    const validateForm = (): boolean => {
+        const newErrors: Partial<Record<'full_name' | 'phone_number' | 'date_of_birth' | 'sex', string>> = {};
+        if (!formData.full_name.trim()) newErrors.full_name = 'Họ và tên là bắt buộc';
+        if (!formData.phone_number) newErrors.phone_number = 'Số điện thoại là bắt buộc';
+        if (!formData.date_of_birth) newErrors.date_of_birth = 'Ngày sinh là bắt buộc';
+        if (!formData.sex) newErrors.sex = 'Giới tính là bắt buộc';
+        // Thêm validation khác nếu cần
+  
+        setErrors(newErrors);
+        return Object.keys(newErrors).length === 0; // True nếu không có lỗi
+    };
+
     const handleAvatarChange = (event: ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
         if(file && file.type.startsWith('image/')){
@@ -92,7 +140,7 @@ function ProfileManager (){
             reader.onloadend = () => { setAvatarPreview(reader.result as string); };
             reader.readAsDataURL(file);
         } else {
-            if (file) notify({ message: 'Vui lòng chọn file ảnh.', severity: 'warning' });
+            if (!file) notify({ message: 'Vui lòng chọn file ảnh.', severity: 'warning' });
             setFormData(prev => ({ ...prev, avatarFile: null }));
             setAvatarPreview(null);
             event.target.value = '';
@@ -123,12 +171,33 @@ function ProfileManager (){
 
     const handleSubmit = async(event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        console.log("formData: ",formData);
+        if(!validateForm()){
+            return;
+        }
+        const data = new FormData();
+        data.append('full_name', formData.full_name);
+        data.append('sex', formData.sex);
+        if(formData.date_of_birth) data.append('date_of_birth', formData.date_of_birth.toISOString());
+        data.append('phone_number', formData.phone_number);
+        if(formData.address_work) data.append('address_work', formData.address_work);
+        if(formData.email) data.append('email', formData.email);
+        if(formData.address) data.append('address', formData.address)
+        try {
+            const res = await updateUserProfile(userId, data)
+            console.log("res: ", res);
+            
+        } catch (error) {
+            
+        }
+
+        // const { role, ...payload} = data;
+        // console.log("payload: ",payload); 
+        
         
     }
     
     return(
-        <Box sx={{ p: 1, m:1, minHeight: "100%"}}>
+        <Box sx={{ p: 0.5, m:1, minHeight: "100%"}}>
             <Grid container spacing={2} alignItems="stretch" >
                 {/* Hồ sơ người dùng */}
                 <Grid size={{ xs: 12, md:3}}>
@@ -201,8 +270,8 @@ function ProfileManager (){
                                                 onChange={handleCustomInputChange}
                                                 placeholder="Họ và tên"
                                                 sx={{ mt: 0 }}
-                                                // error={!!errors.title}
-                                                // helperText={errors.title}
+                                                error={!!errors.full_name}
+                                                helperText={errors.full_name}
                                                 margin="dense"
                                             />
                                         </Grid>
@@ -216,8 +285,8 @@ function ProfileManager (){
                                                 onChange={handleCustomInputChange}
                                                 placeholder="Nam/nữ"
                                                 sx={{ mt: 0 }}
-                                                // error={!!errors.title}
-                                                // helperText={errors.title}
+                                                error={!!errors.sex}
+                                                helperText={errors.sex}
                                                 margin="dense"
                                             />
                                         </Grid>
@@ -232,8 +301,8 @@ function ProfileManager (){
                                                 placeholder="Ngày sinh"
                                                 sx={{ mt: 0 }}
                                                 maxDate={eighteenYearsAgo}
-                                                // error={!!errors.title}
-                                                // helperText={errors.title}
+                                                error={!!errors.date_of_birth}
+                                                helperText={errors.date_of_birth}
                                                 margin="dense"
                                             />
                                         </Grid>
@@ -277,8 +346,8 @@ function ProfileManager (){
                                                 onChange={handleCustomInputChange}
                                                 placeholder="Điện thoại"
                                                 sx={{ mt: 0 }}
-                                                // error={!!errors.title}
-                                                // helperText={errors.title}
+                                                error={!!errors.phone_number}
+                                                helperText={errors.phone_number}
                                                 margin="dense"
                                             />
                                         </Grid>
